6e909e867edfa6b5407e1d65332287844d8fff2d
Seb Glazebrook <sebastian.glazebrook@redbubble.com>
2018-06-07 21:33:02 +1000

    Added hot reloading in development
    Why?
    
    Starting and stopping the server for all file changes is slow

--- app.rb
+++ app.rb
@@ -1,14 +1,19 @@
 require 'sinatra/base'
+require "sinatra/reloader"
 
 require "ostruct"
 def posts_data
   [
     OpenStruct.new({ title: "First post", content: "Here's the first post", tags: ["first tag", "second tag"] }),
-    OpenStruct.new({ title: "Second post", content: "Here's the second post", tags: ["second tag", "third tag"] })
+    OpenStruct.new({ title: "Second post", content: "Here's the second post", tags: ["second tag", "third tag"] }),
+    OpenStruct.new({ title: "Third post", content: "Here's the second post", tags: ["forth tag", "third tag"] })
   ]
 end
 
 class App < Sinatra::Base
+  configure :development do
+    register Sinatra::Reloader
+  end
 
   get '/' do
     erb :index, locals: { posts: posts_data }
