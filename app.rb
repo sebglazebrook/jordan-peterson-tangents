a45c3d9e00e0502706b6c0b54883c150f74cdca9
Seb Glazebrook <sebastian.glazebrook@redbubble.com>
2018-06-07 21:44:29 +1000

    Improved the test data
    Why?
    
    It was missing some important bits.

--- app.rb
+++ app.rb
@@ -4,9 +4,22 @@ require "sinatra/reloader"
 require "ostruct"
 def posts_data
   [
-    OpenStruct.new({ title: "First post", content: "Here's the first post", tags: ["first tag", "second tag"] }),
-    OpenStruct.new({ title: "Second post", content: "Here's the second post", tags: ["second tag", "third tag"] }),
-    OpenStruct.new({ title: "Third post", content: "Here's the second post", tags: ["forth tag", "third tag"] })
+    OpenStruct.new({
+      title: "Jordan Peterson on Parenting",
+      synopsis: "In between talking about the Lion King, Jordan Peterson enlightens us on how you should handle 3 year olds when they are not sharing their toys.",
+      video: "https://www.youtube.com/watch?v=Us979jCjHu8&t=1573s",
+      start_time: "11:45",
+      duration: "4 minutes",
+      tags: ["parenting", "life"]
+    }),
+    OpenStruct.new({
+      title: "Jordan Peterson on Nazi Germany",
+      synopsis: "Peterson tells us why WW2 happened.",
+      video: "https://www.youtube.com/watch?v=Us979jCjHu8&t=1573s",
+      start_time: "04:01",
+      duration: "21 minutes",
+      tags: ["ww2", "war", "Germany"]
+    }),
   ]
 end
 
@@ -16,7 +29,7 @@ class App < Sinatra::Base
   end
 
   get '/' do
-    erb :index, locals: { posts: posts_data }
+    erb :index, locals: { site_title: "Hello World!", posts: posts_data }
   end
 
   run! if app_file == $0
