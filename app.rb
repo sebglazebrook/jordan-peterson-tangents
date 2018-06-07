f033820010bb1445bd002400c59f9686b4f3c258
Seb Glazebrook <sebastian.glazebrook@redbubble.com>
2018-06-07 21:27:57 +1000

    Homepage html skeleton with some test data
    Why?
    
    What to start to understand the UI and the data structure that will
    support it.

--- app.rb
+++ app.rb
@@ -1,9 +1,17 @@
 require 'sinatra/base'
 
+require "ostruct"
+def posts_data
+  [
+    OpenStruct.new({ title: "First post", content: "Here's the first post", tags: ["first tag", "second tag"] }),
+    OpenStruct.new({ title: "Second post", content: "Here's the second post", tags: ["second tag", "third tag"] })
+  ]
+end
+
 class App < Sinatra::Base
 
   get '/' do
-    'Hello world!'
+    erb :index, locals: { posts: posts_data }
   end
 
   run! if app_file == $0
