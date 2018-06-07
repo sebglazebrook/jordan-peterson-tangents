c41add8aa372675febe1cbf424938ecc40cef667
Seb Glazebrook <sebastian.glazebrook@redbubble.com>
2018-06-07 22:37:50 +1000

    Added basic styling using style tag in header.
    Why?
    
    No need for a separate style at this stage. Just need to get something
    that looks ok and then see how it evolves.

--- app.rb
+++ app.rb
@@ -7,7 +7,7 @@ def posts_data
     OpenStruct.new({
       title: "Jordan Peterson on Parenting",
       synopsis: "In between talking about the Lion King, Jordan Peterson enlightens us on how you should handle 3 year olds when they are not sharing their toys.",
-      video: "https://www.youtube.com/watch?v=Us979jCjHu8&t=1573s",
+      video: "https://www.youtube.com/embed/Us979jCjHu8?start=110",
       start_time: "11:45",
       duration: "4 minutes",
       tags: ["parenting", "life"]
@@ -15,7 +15,7 @@ def posts_data
     OpenStruct.new({
       title: "Jordan Peterson on Nazi Germany",
       synopsis: "Peterson tells us why WW2 happened.",
-      video: "https://www.youtube.com/watch?v=Us979jCjHu8&t=1573s",
+      video: "https://www.youtube.com/embed/Us979jCjHu8?start=110",
       start_time: "04:01",
       duration: "21 minutes",
       tags: ["ww2", "war", "Germany"]
